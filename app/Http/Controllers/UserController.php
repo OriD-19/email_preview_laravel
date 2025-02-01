cbee21ee828498ba4def3f5857d3b5465fbefd78
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-02-01 14:26:24 -0600

    feature: create and query users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $usernameQuery = $request->query('username');
+        $emailQuery = $request->query('email');
+
+        $users = DB::table('users');
+
+        if ($usernameQuery) {
+            $users->where('username', 'like', '%'. $usernameQuery. '%');
+        }
+
+        if ($emailQuery) {
+            $users->where('email', 'like', '%'. $emailQuery. '%');
+        }
+
+        $res = [];
+
+        foreach($users->get() as $user) {
+            array_push($res, UserResource::make($user));
+        }
+
+        return response()->json($res);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Str::random(8);
+
+        $user = User::create($data);
+
+        return response()->json(UserResource::make($user), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,13 +7,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         // Desactivar validación CSRF para la ruta especificada
         $middleware->validateCsrfTokens(except: [
-            '/api/v1/email/preview'
+            '/api/v1/email/preview',
+            '/api/v1/users',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
